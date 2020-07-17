a068dc038e231e8d5beed5b1ecb60276f3150899
Sho Sato <sho.sato.14@ucl.ac.uk>
2020-07-17 23:02:50 +0100

    Add recursions

---
+++ Algorithms/Recursions/CollectionRecursion.cs
@@ -0,0 +1,32 @@
+﻿namespace Algorithms.Recursions
+{
+    using System.Collections.Generic;
+    using System.Linq;
+
+    public static class CollectionRecursion
+    {
+        public static List<int> Execute(int[] items)
+        {
+            var result = new List<int>();
+
+            void helper(Stack<int> stack)
+            {
+                if (stack.Count() == 0)
+                {
+                    return;
+                }
+
+                var item = stack.Pop();
+                if (item % 2 == 0)
+                {
+                    result.Add(item);
+                }
+
+                helper(stack);
+            }
+
+            helper(new Stack<int>(items));
+            return result;
+        }
+    }
+}
---
+++ Algorithms/Recursions/NonCollectionRecursion.cs
@@ -0,0 +1,15 @@
+﻿namespace Algorithms.Recursions
+{
+    public static class NonCollectionRecursion
+    {
+        public static int Execute(int item)
+        {
+            if (item <= 1)
+            {
+                return item;
+            }
+
+            return item * Execute(item - 1);
+        }
+    }
+}

fc75b42cc81582854bec73df9bdb474eadca775d
Sho Sato <sho.sato.14@ucl.ac.uk>
2020-07-18 23:59:49 +0100

    Add selection sort

---
+++ Algorithms/Sorts/SelectionSort.cs
@@ -0,0 +1,41 @@
+﻿namespace Algorithms.Sorts
+{
+    public static class SelectionSort
+    {
+        public static int[] Sort(int[] items)
+        {
+            // Define the first index of the unsorted items to which the minimum item can be pushed
+            for (var i = 0; i < items.Length; i++)
+            {
+                // Pick the first item of the unsorted items as minimum
+                var min = i;
+                // Iterate over all unsorted items
+                for (var j = i + 1; j < items.Length; j++)
+                {
+                    // If the current item is less than the current minimum item
+                    // keep track of it
+                    if (items[j] < items[min])
+                    {
+                        min = j;
+                    }
+                }
+
+                // If the minimum item has changed from the first item
+                // swap it to the beginning of the unsorted items
+                if (i != min)
+                {
+                    Swap(items, i, min);
+                }
+            }
+
+            return items;
+        }
+
+        public static void Swap(int[] items, int index1, int index2)
+        {
+            var temp = items[index1];
+            items[index1] = items[index2];
+            items[index2] = temp;
+        }
+    }
+}

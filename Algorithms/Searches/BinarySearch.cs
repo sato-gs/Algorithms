9a006b2401ca67246bc76769bbb4a757c24b7162
Sho Sato <sho.sato.14@ucl.ac.uk>
2020-07-18 14:24:50 +0100

    Add search algorithms - linear and binary

---
+++ Algorithms/Searches/BinarySearch.cs
@@ -0,0 +1,30 @@
+﻿namespace Algorithms.Searches
+{
+    public static class BinarySearch
+    {
+        public static int Search(int[] items, int target)
+        {
+            var low = 0;
+            var high = items.Length - 1;
+
+            while (low <= high)
+            {
+                var middle = (low + high) / 2;
+                if (target == items[middle])
+                {
+                    return middle;
+                }
+                else if (target < items[middle])
+                {
+                    high = middle - 1;
+                }
+                else
+                {
+                    low = middle + 1;
+                }
+            }
+
+            return -1;
+        }
+    }
+}
---
+++ Algorithms/Searches/LinearSearch.cs
@@ -0,0 +1,18 @@
+﻿namespace Algorithms.Searches
+{
+    public static class LinearSearch
+    {
+        public static int Search(int[] items, int target)
+        {
+            for (var i = 0; i < items.Length; i++)
+            {
+                if (items[i] == target)
+                {
+                    return i;
+                }
+            }
+
+            return -1;
+        }
+    }
+}

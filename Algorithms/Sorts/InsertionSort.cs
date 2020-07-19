06e9e781bbb53a83ac2a0068bb4563f02cd9abf7
Sho Sato <sho.sato.14@ucl.ac.uk>
2020-07-19 02:37:33 +0100

    Add insertion sort

---
+++ Algorithms/Sorts/InsertionSort.cs
@@ -0,0 +1,28 @@
+﻿namespace Algorithms.Sorts
+{
+    public static class InsertionSort
+    {
+        public static int[] Sort(int[] items)
+        {
+            // Iterate over the unsorted items (on the right side)
+            for (var i = 1; i < items.Length; i++)
+            {
+                // Compare it with the sorted items (on the left side) backwards
+                // Keep swapping the position of two items until the current item is inserted into the correct position
+                for (var j = i - 1; j > 0 && items[j] < items[j - 1]; j--)
+                {
+                    Swap(items, j, j - 1);
+                }
+            }
+
+            return items;
+        }
+
+        public static void Swap(int[] items, int index1, int index2)
+        {
+            var temp = items[index1];
+            items[index1] = items[index2];
+            items[index2] = temp;
+        }
+    }
+}

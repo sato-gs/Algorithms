03501459d94e8eb87f0ee7b7ecba88c38d9a6966
Sho Sato <sho.sato.14@ucl.ac.uk>
2020-08-06 23:49:22 +0100

    Add shell sort

--- Algorithms/Sorts/InsertionSort.cs
+++ Algorithms/Sorts/InsertionSort.cs
@@ -7,23 +7,20 @@
             // Iterate over the unsorted items (on the right side)
             for (var i = 1; i < items.Length; i++)
             {
-                // Compare it with the sorted items (on the left side) backwards
-                // Keep swapping the position of two adjacent items until the current item is inserted into the correct position
-                for (var j = i - 1; j > 0 && items[j] < items[j - 1]; j--)
+                var cur = items[i];
+                int j;
+                // Compare the current item with the sorted items (on the left side) backwards
+                // Keep shifting items smaller than the current item towards right
+                for (j = i; j > 0 && items[j - 1] > cur; j--)
                 {
-                    Swap(items, j, j - 1);
+                    items[j] = items[j - 1];
                 }
+
+                // Insert the current item into the correct position
+                items[j] = cur;
             }
 
             return items;
-        }
-
-        // Define the helper function to swap items
-        public static void Swap(int[] items, int index1, int index2)
-        {
-            var temp = items[index1];
-            items[index1] = items[index2];
-            items[index2] = temp;
         }
     }
 }
---
+++ Algorithms/Sorts/ShellSort.cs
@@ -0,0 +1,30 @@
+﻿namespace Algorithms.Sorts
+{
+    public static class ShellSort
+    {
+        public static int[] Sort(int[] items)
+        {
+            // Start with a big gap and continue until the gap becomes 0
+            for (var interval = items.Length / 2; interval > 0; interval /= 2)
+            {
+                // Iterate over items starting at the gap position
+                for (var i = interval; i < items.Length; i++)
+                {
+                    var cur = items[i];
+                    int j;
+                    // Compare the current item with items at the gap positions (on the left side) backwards
+                    // Keep shifting items smaller than the current item towards right
+                    for (j = i; j >= interval && items[j - interval] > cur; j -= interval)
+                    {
+                        items[j] = items[j - interval];
+                    }
+
+                    // Insert the current item into the correct position
+                    items[j] = cur;
+                }
+            }
+
+            return items;
+        }
+    }
+}

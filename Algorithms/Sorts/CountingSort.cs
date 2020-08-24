1edfc945c089dc065c86dac4e2072bdaf98ebffa
Sho Sato <sho.sato.14@ucl.ac.uk>
2020-08-24 13:42:28 +0100

    Fix counting sort

--- Algorithms/Sorts/CountingSort.cs
+++ Algorithms/Sorts/CountingSort.cs
@@ -6,7 +6,7 @@
     {
         public static int[] Sort(int[] items)
         {
-            // Work out minimum and maximum numbers
+            // Work out minimum and maximum items
             var min = items.FirstOrDefault();
             var max = items.FirstOrDefault();
             for (var i = 0; i < items.Length; i++)
@@ -21,20 +21,20 @@
                 }
             }
 
-            // Count the frequency of numbers in the range of min - max
+            // Count the number of times each item appears in the range of min - max
             var count = new int[max - min + 1];
             for (var j = 0; j < items.Length; j++)
             {
                 count[items[j] - min]++;
             }
 
-            // Sort numbers based on the count array
+            // Sort items based on the count array
             var counter = 0;
             for (var k = min; k <= max; k++)
             {
                 while (count[k - min]-- > 0)
                 {
-                    items[counter++] = count[k];
+                    items[counter++] = k;
                 }
             }
 
